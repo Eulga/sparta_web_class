40d6483fe17770529ec315448597697dc04259ef
Eulga <kimyeoun10@gmail.com>
2023-05-30 09:50:14 +0900

    Week 5 Task

--- projects/fan/app.py
+++ projects/fan/app.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.r2nnoby.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route("/guestbook", methods=["POST"])
 def guestbook_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+    db.fan.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
 
 @app.route("/guestbook", methods=["GET"])
 def guestbook_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    all_comments = list(db.fan.find({},{'_id': False}))
+    return jsonify({'result': all_comments})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
