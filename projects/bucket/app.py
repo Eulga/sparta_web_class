5e1ae5d8e085452df3b843d9ca6649db7c256709
Eulga <kimyeoun10@gmail.com>
2023-06-06 17:26:17 +0900

    bucket 완료 버튼 추가, 배포 테스트

--- projects/bucket/app.py
+++ projects/bucket/app.py
@@ -16,9 +16,25 @@ def home():
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
     bucket_receive = request.form['bucket_give']
-    doc = {'bucket': bucket_receive}
+
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
+    doc = {
+        'num': count,  # 버킷 등록 시, db에서 특정 버킷을 찾기 위해 'num' 이라는 고유 값 부여
+        'bucket': bucket_receive,
+        'done': 0  # 'done' key값을 추가 해 각 버킷의 완료 상태 구분(0 = 미완료, 1 = 완료)
+    }
     db.bucket.insert_one(doc)
     return jsonify({'msg': '저장 완료!'})
+
+
+@app.route("/bucket_complete", methods=["POST"])
+def bucket_complete_post():
+    count = request.form['count_give']
+
+    db.bucket.update_one({'num': int(count)}, {'$set': {'done': 1}})
+
+    return jsonify({'msg': '수정 완료!'})
 
 
 @app.route("/bucket", methods=["GET"])
--- projects/fan/app.py
+++ projects/fan/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://sparta:test@cluster0.r2nnoby.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route("/guestbook", methods=["POST"])
 def guestbook_post():
@@ -21,10 +25,39 @@ def guestbook_post():
 
     return jsonify({'msg': '저장 완료!'})
 
+
 @app.route("/guestbook", methods=["GET"])
 def guestbook_get():
-    all_comments = list(db.fan.find({},{'_id': False}))
+    all_comments = list(db.fan.find({}, {'_id': False}))
     return jsonify({'result': all_comments})
+
+
+@app.route("/editcomment", methods=["POST"])
+def editcomment():
+    name_receive = request.form['name']
+    old_comment_receive = request.form['oldComment']
+    new_comment_receive = request.form['newComment']
+
+    db.fan.update_one({
+        'name': name_receive,
+        'comment': old_comment_receive
+    }, {'$set': {'comment': new_comment_receive}})
+
+    return jsonify({'msg': '수정 완료!'})
+
+
+@app.route("/deletecomment", methods=["POST"])
+def deletecomment():
+    name_receive = request.form['name']
+    comment_receive = request.form['comment']
+
+    db.fan.delete_one({
+        'name': name_receive,
+        'comment': comment_receive
+    })
+
+    return jsonify({'msg': '삭제 완료!'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
